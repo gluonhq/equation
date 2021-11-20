fc9985fd29b988b97cfbead887a944e72e6f5189
Johan Vos <johan@lodgon.com>
2021-11-20 20:22:18 +0100

    add message class

---
+++ src/main/java/com/gluonhq/equation/model/Message.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2021 Gluon
+ *
+ * Licensed according to the LICENSE file in this repository.
+ */
+package com.gluonhq.equation.model;
+
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author johan
+ */
+public class Message {
+
+    String senderUuid;
+    String content;
+    long timestamp;
+    String receiverUuid;
+    List<Path> attachment = new LinkedList<>();
+    
+    public Message () {
+        
+    }
+    
+    public Message senderUuid(String v) {
+        this.senderUuid = v;
+        return this;
+    }
+    
+    public Message content(String v) {
+        this.content = v;
+        return this;
+    }
+    
+    public Message receiverUuid(String v) {
+        this.receiverUuid = v;
+        return this;
+    }
+    
+    public Message timestamp(long v) {
+        this.timestamp = v;
+        return this;
+    }
+    
+    public Message attachment(Path p) {
+        attachment.add(p);
+        return this;
+    }
+    
+    public String getSenderUuid() {
+        return senderUuid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getReceiverUuid() {
+        return receiverUuid;
+    }
+
+    public List<Path> getAttachment() {
+        return attachment;
+    }
+    
+}
